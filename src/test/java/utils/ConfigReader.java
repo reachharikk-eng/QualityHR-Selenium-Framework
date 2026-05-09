297e08e3ebdc5eeecc22660d69090006e051e0ab
reachharikk-eng <reachharikk@gmail.com>
2026-05-09 12:22:58 +0530

    Added configuration management setup

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,4 +1,39 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+
+    Properties properties;
+
+    public ConfigReader() {
+
+        try {
+
+            FileInputStream file =
+                    new FileInputStream("src/test/resources/config.properties");
+
+            properties = new Properties();
+
+            properties.load(file);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public String getBrowser() {
+        return properties.getProperty("browser");
+    }
+
+    public String getBaseUrl() {
+        return properties.getProperty("baseUrl");
+    }
+
+    public int getTimeout() {
+        return Integer.parseInt(properties.getProperty("timeout"));
+    }
 }
