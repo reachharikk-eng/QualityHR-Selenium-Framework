56a92fc43eecd8d418dd0ffe639ba68a6a7c1f6d
reachharikk-eng <reachharikk@gmail.com>
2026-05-09 13:30:28 +0530

    Added BaseTest setup with TestNG lifecycle

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,24 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+
+    public void setup() {
+
+        driver = DriverFactory.initializeDriver();
+    }
+
+    @AfterMethod
+
+    public void tearDown() {
+
+        DriverFactory.quitDriver();
+    }
 }
