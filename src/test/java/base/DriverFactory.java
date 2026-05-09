25a0b3473eeface3ac1bb78c38910c6b42c90f96
reachharikk-eng <reachharikk@gmail.com>
2026-05-09 13:17:49 +0530

    Implemented DriverFactory setup

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -1,4 +1,55 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import utils.ConfigReader;
+
+import java.time.Duration;
+
 public class DriverFactory {
+
+    static WebDriver driver;
+
+    public static WebDriver initializeDriver() {
+
+        ConfigReader config = new ConfigReader();
+
+        String browser = config.getBrowser();
+
+        if (browser.equalsIgnoreCase("chrome")) {
+
+            WebDriverManager.chromedriver().setup();
+
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+
+            WebDriverManager.firefoxdriver().setup();
+
+            driver = new FirefoxDriver();
+        }
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(config.getTimeout()));
+
+        driver.get(config.getBaseUrl());
+
+        return driver;
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void quitDriver() {
+
+        if (driver != null) {
+
+            driver.quit();
+        }
+    }
 }
